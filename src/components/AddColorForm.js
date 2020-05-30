12e5cd07431f91ea69f80bfadac06fecbfc3a210
TJ Hooker <tyrelhooker@gmail.com>
2020-05-29 23:04:14 -0400

    Create useRef hook to handle input refs on title and color and reset and focus back in form. Separate onSubmit handler function to own function

--- src/components/AddColorForm.js
+++ src/components/AddColorForm.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const AddColorForm = () => {
+
+  const titleInput = useRef('');
+  const colorInput = useRef('#000000');
+  
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(`New Color: ${titleInput.current.value} ${colorInput.current.value}`);
+    titleInput.current.value = '';
+    colorInput.current.value = '#000000';
+    titleInput.current.focus();
+  }
+
   return (
-    <form onSubmit={e=>e.preventDefault()}>
-      <input 
+    <form onSubmit={submit}>
+      <input ref={titleInput}
         type='text'
         placeholder='color title...' 
         required />
-      <input
+      <input ref={colorInput}
         type='color'
         required/>
       <button>ADD</button>
