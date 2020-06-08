b8759b775e50d61ee4fcf7753feeae8633d0d616
TJ Hooker <tyrelhooker@gmail.com>
2020-06-07 23:08:45 -0400

    Add comments explaining how the function passed to Star component works

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -2,12 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Star from './Star';
 
-const StarRating = ({ 
-  totalStars = 5, 
-  starsSelected = 0, 
-  onRate2 = f => f 
+const StarRating = ({
+  totalStars = 5,
+  starsSelected = 0,
+  onRate2 = f => f
 }) => {
-
   return (
     <div className='star-rating'>
       {/* Can use Array(totalStars).fill() OR Array.from({length: totalStars} in lieu of spread ... operator */}
@@ -15,7 +14,7 @@ const StarRating = ({
         <Star
           key={i}
           selected={i < starsSelected}
-          // This handler sets the star rating that is then passed up to the App component through the ColorList prop
+          /* This onClick handler sets the star rating that is then passed up to the App component when invoked by the Star onClick. The star rating is passed up through the ColorList component and passes i + 1 as the argument which is then passed as the second parameter in the onRate function call in App component. */
           onClick={() => onRate2(i + 1)}
         />
       ))}
@@ -24,7 +23,6 @@ const StarRating = ({
       </p>
     </div>
   );
-  
 };
 
 StarRating.propTypes = {
